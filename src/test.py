112096a561cc8ec4468d0c504f66f50efdbb87fe
ianknowles <ian@imknowles.co.uk>
2017-10-23 06:26:18 +0100

    more test cases

--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ def test_task():
 	# w.pack()
 
 	data_path = os.path.join(wrangler.path, 'data')
-	#total += wrangler.process_pdf(db_pathname, os.path.join(data_path, 'bis', 'bis-ministerial-expenses-january-march-2015.pdf'))
+	total += wrangler.process_pdf(db_pathname, os.path.join(data_path, 'bis', 'bis-ministerial-expenses-january-march-2015.pdf'))
 	#total += wrangler.process_pdf(db_pathname, os.path.join(data_path, 'bis', 'bis-ministerial-expenses-july-september-2014.pdf'))
 	# process_file(db_pathname, os.path.join(data_path, 'moj', '2015q1.pdf'))
 
@@ -26,9 +26,22 @@ def test_task():
 	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'clg', '2011q2.csv'))
 	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'dfe', '2016-dfe-Q3 (july-september) 16-12-2016.csv'))
 	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'clg', '2012q4.csv'))
-	#TODO has a weird multi-line format that isn't captured
 	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'clg', '2014q2.csv'))
 	#co 2010q4
+	#TODO dropping meetings from here due to weird use of line breaks instead of commas
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'ho', '2012q1.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'clg', 'Senior_Officials_meetings_January_to_March_2016.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'bis', 'bis-ministerial-hospitality-jan-mar-2014.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'dwp', '2012q1.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'dcms', '2011q1.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'pmo', '2017-pmo-Q1 (january-march) 30-06-2017.csv'))
+	#TODO bad multi-line formatting
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'decc', '2011q3_2.csv'))
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'decc', '2011q3_3.csv'))
+
+	#TODO some meetings have no date attached
+	total += CSV.table_transform.process(db_pathname, os.path.join(data_path, 'dft', '2014q1.csv'))
+
 	wrangler.logger.info(str(total) + ' total meetings committed')
 	# root.mainloop()
 
